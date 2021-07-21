2ff1b8be86244b28959fe9f257641d0650d08a18
TerwalEast <terwaleast@gmail.com>
2021-07-21 11:05:48 +0800

    init

---
+++ src/main/java/com/noname/HWBlog/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.noname.HWBlog.controller;
+
+import com.noname.HWBlog.model.User;
+import com.noname.HWBlog.service.LoginService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class UserController {
+
+    @Resource
+    LoginService loginService;
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String doLogin(HttpServletRequest request, User user, Model model) {
+        if (loginService.login(user.getId(), user.getPassword()))
+        {
+            return "";
+        }
+        else
+            {
+            model.addAttribute("error", "用户名或密码错误");
+            return "";
+        }
+    }
+
+
+
+
+
+
+}
--- src/main/java/com/noname/HWBlog/service/LoginService.java
+++ src/main/java/com/noname/HWBlog/service/LoginService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 
 @Service
-public class loginService {
+public class LoginService {
 
     @Resource
     UserDao userDao;
